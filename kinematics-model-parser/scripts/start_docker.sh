af6a21c702881b2e70153a92b6b60028e90bcdb0
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2023-09-22 14:21:57 +0200

    pass args to start docker container

--- kinematics-model-parser/scripts/start_docker.sh
+++ kinematics-model-parser/scripts/start_docker.sh
@@ -2,15 +2,18 @@
 
 # TODO: is this always needed?
 # check if exists and is running -- else script fails
-docker container stop urdf-toolchain
-docker container rm urdf-toolchain
+# $1 = docker image name
+# $2 = docker container name
 
-docker run -it --name urdf-toolchain --net host -p 5500:5500 -d urdf-toolchain:latest
+docker container stop $2
+docker container rm $2
 
-docker exec -it -d urdf-toolchain  /bin/bash -c "flask --app app run --host 0.0.0.0 --port 5500"
+docker run -it --name $2 --net host -p 5500:5500 -d $1
 
-docker exec -it urdf-toolchain /bin/bash -c \
+docker exec -it -d $2  /bin/bash -c "flask --app app run --host 0.0.0.0 --port 5500"
+
+docker exec -it $2 /bin/bash -c \
     ". /app/kinematic_components_web_app/static/moveit2_ws/install/setup.sh \
         && ros2 run kinematics_model_generator batch_model_generator.py \
-            ./static/moveit2_ws/src/urdf-model/kinematics-model-parser/kinematics_model_generator/config/component_list.yaml \
+            ./static/moveit2_ws/src/urdf-model/component_list.yaml \
             ./static/moveit2_ws/src/urdf-model/kinematics-model-parser/kinematics_model_generator/models"
